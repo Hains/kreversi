Hains/kreversi

commit edb8eae743bd87c9d7fabe71fcd5567d4b9a060b

Fix theme/icon on windows

src/main.cpp
@@ -12,15 +12,32 @@
 #include <KLocalizedString>
 #include <KCrash>
 #include <KDBusService>
+#define HAVE_KICONTHEME __has_include(<KIconTheme>)
+#if HAVE_KICONTHEME
+#include <KIconTheme>
+#endif
 
+#define HAVE_STYLE_MANAGER __has_include(<KStyleManager>)
+#if HAVE_STYLE_MANAGER
+#include <KStyleManager>
+#endif
 #include "highscores.h"
 #include "mainwindow.h"
 #include "kreversi_version.h"
 
 int main(int argc, char **argv)
 {
+#if HAVE_KICONTHEME
+    KIconTheme::initTheme();
+#endif
     QApplication application(argc, argv);
-
+#if HAVE_STYLE_MANAGER
+    KStyleManager::initStyle();
+#else // !HAVE_STYLE_MANAGER
+#if defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+    QApplication::setStyle(QStringLiteral("breeze"));
+#endif // defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+#endif // HAVE_STYLE_MANAGER
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("kreversi"));
 
     KAboutData aboutData(QStringLiteral("kreversi"), i18n("KReversi"),

.kde-ci.yml
@@ -17,6 +17,7 @@ Dependencies:
     'frameworks/kjobwidgets': '@latest-kf6'
     'frameworks/kxmlgui': '@latest-kf6'
     'frameworks/kwidgetsaddons': '@latest-kf6'
+    'frameworks/kiconthemes': '@latest-kf6'
     'games/libkdegames': '@same'
 
 Options:

CMakeLists.txt
@@ -53,6 +53,7 @@ find_package(KF6 ${KF_MIN_VERSION} REQUIRED COMPONENTS
     KIO
     XmlGui
     WidgetsAddons
+    IconThemes
 )
 
 find_package(KDEGames6 6.0.0 REQUIRED)

src/CMakeLists.txt
@@ -67,6 +67,7 @@ target_link_libraries(kreversi
     KF6::XmlGui
     Qt6::QuickWidgets
     Qt6::Svg
+    KF6::IconThemes
 )
 
 install(TARGETS kreversi  ${KDE_INSTALL_TARGETS_DEFAULT_ARGS})

commit 907cc226e595e3f058748d7431dbc2d62248a2cf

main.cpp: Fix broken theme (Gnome)
Introduced with commit:
https://github.com/KDE/kreversi/commit/edb8eae743bd87c9d7fabe71fcd5567d4b9a060b

src/main.cpp
@@ -27,9 +27,6 @@
 
 int main(int argc, char **argv)
 {
-#if HAVE_KICONTHEME
-    KIconTheme::initTheme();
-#endif
     QApplication application(argc, argv);
 #if HAVE_STYLE_MANAGER
     KStyleManager::initStyle();